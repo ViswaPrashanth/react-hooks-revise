ca46e06656dee5e77536f65e0b765a2d1288e2c6
ViswaPrasanth <85486741+ViswaPrashanth@users.noreply.github.com>
2023-02-28 18:07:36 +0530

    useEffect Data Fetching Three

--- src/components/UseEffectDataFetching.js
+++ src/components/UseEffectDataFetching.js
@@ -3,23 +3,30 @@ import axios from "axios";
 
 const UseEffectDataFetching = () => {
   const [post, setPost] = useState([]);
-  const [id, setId] = useState(1)
+  const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+  };
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((res) => {
         console.log(res);
-        setPost(res.data)
+        setPost(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [id]);
+  }, [idFromButtonClick]);
 
   return (
     <div>
-        <input type="text" value={id} onChange={e => setId(e.target.value)}/>
-        <p>{post.title}</p>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fetch Data
+      </button>
+      <p>{post.title}</p>
       {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
